8f5166b5b4ab799c6449db44604ec16c115cbc4d
Nitesh Sinha <mitnitesh1@gmail.com>
2024-03-06 15:37:55 +0530

    Convert sorted list to BST

---
+++ tree/sorted_list_to_bst.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from tree.definition.tree import TreeNode
+from tree.btree_printer import BTreePrinter
+
+
+class BSTCreator:
+    def sortedArrayToBST(self, nums: list[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        return root
+
+
+if __name__ == "__main__":
+    bst = BSTCreator()
+    bst_root = bst.sortedArrayToBST([-10,-3,0,5,9])
+    BTreePrinter.print_tree(bst_root)
+
+
+    ## Another algo:
+    # def sortedArrayToBST(self, nums: list[int]) -> Optional[TreeNode]:
+    #     return self.create_balanced_tree(nums, 0, len(nums) - 1)
+    #
+    # def create_balanced_tree(self, nums, start, end) -> Optional[TreeNode]:
+    #     if end < start:
+    #         return None
+    #
+    #     mid = start + (end - start) // 2
+    #     root = TreeNode(nums[mid])
+    #     root.left = self.create_balanced_tree(nums, start, mid - 1)
+    #     root.right = self.create_balanced_tree(nums, mid + 1, end)
+    #     return root
